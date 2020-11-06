e24973fe5f9de21b9a7a64f8e331729dca674dec
trisman2001 <73290031+trisman2001@users.noreply.github.com>
2020-11-06 12:40:27 -0600

    Add files via upload

---
+++ hasil dari program.py
@@ -0,0 +1,10 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/User/Documents/terbaru python/New folder/labspy02.py ====
+PROGRAM BILANGAN TERBESAR DARI 3 BUAH BILANGAN YANG DIINPUTKAN
+Masukan bilangan pertama : 1
+Masukan bilangan kedua   : 2
+Masukan bilangan ketiga  : 3
+3 Terbesar dari 3 bilangan yang diinputkan
+>>> 
---
+++ labspy02.py
@@ -0,0 +1,7 @@
+print("PROGRAM BILANGAN TERBESAR DARI 3 BUAH BILANGAN YANG DIINPUTKAN")
+A= int(input("Masukan bilangan pertama : "))
+B= int(input("Masukan bilangan kedua   : "))
+C= int(input("Masukan bilangan ketiga  : "))
+if A > B and A > C: print(A, 'Terbesar dari 3 bilangan yang diinputkan')
+elif B > A and B > C: print(B, 'Terbesar dari 3 bilangan yang diinputkan')
+else: print(C, 'Terbesar dari 3 bilangan yang diinputkan')
